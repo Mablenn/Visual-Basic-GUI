600137844b5828e9dce1d300ad337fae96d2b023
Mablenn <marcos.broseta@gmail.com>
2023-08-23 19:39:02 +0200

    Add files via upload

---
+++ TestGUI01/PerformDataGridView.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PerformDataGridView
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As DataGridViewCellStyle = New DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As DataGridViewCellStyle = New DataGridViewCellStyle()
+        DgvTest = New DataGridView()
+        Column1 = New DataGridViewTextBoxColumn()
+        Column2 = New DataGridViewTextBoxColumn()
+        Column3 = New DataGridViewTextBoxColumn()
+        Column4 = New DataGridViewTextBoxColumn()
+        CType(DgvTest, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' DgvTest
+        ' 
+        DataGridViewCellStyle1.BackColor = Color.FromArgb(CByte(229), CByte(226), CByte(244))
+        DgvTest.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        DataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle2.BackColor = Color.FromArgb(CByte(109), CByte(122), CByte(224))
+        DataGridViewCellStyle2.Font = New Font("Segoe UI", 12.7F, FontStyle.Regular, GraphicsUnit.Point)
+        DataGridViewCellStyle2.ForeColor = Color.White
+        DataGridViewCellStyle2.Padding = New Padding(10)
+        DataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = DataGridViewTriState.True
+        DgvTest.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle2
+        DgvTest.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DgvTest.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3, Column4})
+        DgvTest.GridColor = SystemColors.ControlDark
+        DgvTest.Location = New Point(50, 33)
+        DgvTest.Name = "DgvTest"
+        DgvTest.RowHeadersWidth = 62
+        DgvTest.RowTemplate.Height = 33
+        DgvTest.Size = New Size(913, 365)
+        DgvTest.TabIndex = 0
+        ' 
+        ' Column1
+        ' 
+        Column1.HeaderText = "Full Name"
+        Column1.MinimumWidth = 8
+        Column1.Name = "Column1"
+        Column1.Width = 150
+        ' 
+        ' Column2
+        ' 
+        Column2.HeaderText = "Age"
+        Column2.MinimumWidth = 8
+        Column2.Name = "Column2"
+        Column2.Width = 150
+        ' 
+        ' Column3
+        ' 
+        Column3.HeaderText = "Job title"
+        Column3.MinimumWidth = 8
+        Column3.Name = "Column3"
+        Column3.Width = 150
+        ' 
+        ' Column4
+        ' 
+        Column4.HeaderText = "Location"
+        Column4.MinimumWidth = 8
+        Column4.Name = "Column4"
+        Column4.Width = 150
+        ' 
+        ' PerformDataGridView
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1030, 450)
+        Controls.Add(DgvTest)
+        Name = "PerformDataGridView"
+        Text = "PerformDataGridView"
+        CType(DgvTest, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents DgvTest As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+End Class
---
+++ TestGUI01/PerformDataGridView.vb
@@ -0,0 +1,3 @@
+﻿Public Class PerformDataGridView
+
+End Class
